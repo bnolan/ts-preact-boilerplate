e0492ff49d0fc7f04df39d058009721041225518
Ben Nolan <bnolan@gmail.com>
2019-03-28 23:14:12 +1300

    Unistore

--- index.tsx
+++ index.tsx
@@ -1,12 +1,35 @@
-/* globals alert */
-
 import { h, render } from 'preact'
+import { createStore, Provider, connect } from 'unistore/full/preact'
 
 /* @jsx h */
 
+let store = createStore({ count: 0 })
+
+let actions = (store) => ({
+  increment (state) {
+    return { count: state.count + 1 }
+  },
+
+  decrement (state) {
+    return { count: state.count - 1 }
+  }
+})
+
+const App = connect('count', actions)(
+  ({ count, increment, decrement }) => (
+    <div>
+      <header>App Header</header>
+      <section>
+        <p>Count: {count}</p>
+        <button onClick={increment}>Increment</button>
+        <button onClick={decrement}>Decrement</button>
+      </section>
+    </div>
+  )
+)
+
 render((
-  <div id='foo'>
-    <span>Hello, world!</span>
-    <button onClick={e => alert('hi!')}>Click Me</button>
-  </div>
+  <Provider store={store}>
+    <App />
+  </Provider>
 ), document.body)
